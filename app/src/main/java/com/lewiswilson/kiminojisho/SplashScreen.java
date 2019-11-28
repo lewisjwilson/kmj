2442f246cfb97410cdddebca45d928a4d827fdb1
lewis.wilson <lewis.j.wilson@outlook.com>
2019-11-29 01:14:31 +0900

    Splash Screen

---
+++ app/src/main/java/com/lewiswilson/kiminojisho/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.lewiswilson.kiminojisho;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
